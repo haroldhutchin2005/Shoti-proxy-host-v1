ca9c245091da878a8ffbd9c3125dc5ccf70ca063
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-09-01 17:23:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://node4.lunes.host:1120' });
+  proxy.web(req, res, { target: 'http://de01.uniplex.xyz:5611' });
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
