0290900d3faa428a4689cc9f9d6ee848fa43cd85
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-10-18 06:06:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://fred.hidencloud.com:25677' });
+  proxy.web(req, res, { target: 'http://venus.hidencloud.com:25589' });
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
